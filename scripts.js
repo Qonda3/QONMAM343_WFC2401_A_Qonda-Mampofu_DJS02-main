25688414f1d539c1d5fc0bd1da40da81c646dbc9
Qonda <138157862+Qonda3@users.noreply.github.com>
2024-05-12 23:12:19 +0200

    incorporate the behavior of crashing the program when non-numeric values

--- scripts.js
+++ scripts.js
@@ -8,20 +8,32 @@ form.addEventListener("submit", (event) => {
   let outputValue;
 
   try {
-    if (divider === "0") {
+
+    const dividendNumber = Number(dividend);
+    const dividerNumber = Number(divider);
+
+    if (isNaN(dividendNumber) || isNaN(dividerNumber)){
+      throw new Error("Non-numeric inputs provided.")
+    }
+
+    if (dividerNumber === "0") {
       throw new Error("Division by zero is not allowed.");
     }
 
     if (!dividend || !divider){
       outputValue = "Division not performed. Both values are required in inputs. Try again"
     } else{
-      outputValue = Math.trunc(dividend / divider);
+      outputValue = Math.trunc(dividendNumber / dividerNumber);
     }
     
   } catch (error) {
     console.error(error.message, error.stack);
-    outputValue = "Division not performed. Invalid number provided. Try again";
+    if (error.message === "Non-numeric inputs provided."){
+      document.body.innerHTML = "Something critical went wrong. Please reload the page";
+    } else {
+      outputValue = "Division not performed. Invalid number provided. Try again";
+    }
   }
 
-  result.innerText = outputValue;
+  result.innerText = outputValue || "";
 });
