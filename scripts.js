83ce042a825da227bc17f234162f1c8233b9baab
Qonda <138157862+Qonda3@users.noreply.github.com>
2024-05-12 22:19:19 +0200

    handle invalid division scenarios

--- scripts.js
+++ scripts.js
@@ -5,10 +5,23 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  let outputValue = Math.trunc(dividend / divider);
+  let outputValue;
 
-  if (!dividend || !divider){
-    outputValue = "Division not performed. Both values are required in inputs. Try again"
+  try {
+    if (divider === "0") {
+      throw new Error("Division by zero is not allowed.");
+    }
+
+    if (!dividend || !divider){
+      outputValue = "Division not performed. Both values are required in inputs. Try again"
+    } else{
+      outputValue = Math.trunc(dividend / divider);
+    }
+    
+  } catch (error) {
+    console.error(error.message, error.stack);
+    outputValue = "Division not performed. Invalid number provided. Try again";
   }
+
   result.innerText = outputValue;
 });
