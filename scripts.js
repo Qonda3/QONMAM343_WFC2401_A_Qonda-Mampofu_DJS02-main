3532e89b4185390d4c04463b73340a8e145f0b48
Qonda <138157862+Qonda3@users.noreply.github.com>
2024-05-12 20:26:33 +0200

    update javascript

--- scripts.js
+++ scripts.js
@@ -1,50 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.querySelector("[data-form]");
-  const result = document.querySelector("[data-result]");
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
 
-  form.addEventListener("submit", handleFormSubmit);
-
-  function handleFormSubmit(event) {
-    event.preventDefault();
-
-    const dividend = getNumberInputValue('dividend');
-    const divider = getNumberInputValue('divider');
-
-    if (isNaN(dividend) || isNaN(divider)) {
-        displayError("Both inputs must be valid numbers.");
-        return;
-    }
-
-    if (divider === 0) {
-      displayError("Division by zero is not allowed.");
-      return;
-    }
-
-    const divisionResult = divideNumbers(dividend, divider);
-
-    if (Number.isInteger(divisionResult)) {
-      displayResult(divisionResult);
-    }else {
-      displayError("Result is not a whole number.");
-    }
-
-  }
-
-  function getNumberInputValue(inputName) {
-    const input = document.querySelector(`input[name="${inputName}"]`);
-    return parseFloat(input.value);
-  }
-
-  function divideNumbers(dividend, divider) {
-    return dividend / divider;
-  }
-
-  function displayResult(result) {
-    resultMessage.textContent = result;
-  }
-
-  function displayError(message) {
-    resultMessage.textContent = message;
-  }
-
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const entries = new FormData(event.target);
+  const { dividend, divider } = Object.fromEntries(entries);
+  result.innerText = dividend / divider;
 });
